de57e21383d0d1ad9fa93ca9a7e4b50923dbfa28
Gustavo Letério <gustavoleterio@gmail.com>
2022-04-19 19:06:36 -0300

    Creating first screen with Styled-Components & React Router DOM

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,14 +1,12 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "../components/Login";
-import Teste from "../components/Teste";
-// import { isAuthenticated } from "../auth/auth";
 
 
 const Router = () => {
     return (<>
         <Routes>
             <Route element={<Login />} path="/" exact />
-            <PrivateRoute element={<Teste />} path="/myspace" exact />
+            <Route element={<Login />} path="/myspace" exact />
         </Routes >
     </>
     )
